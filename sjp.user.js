77c47892001626b45cba9bb51a78b486381f3f62
Piotr Rogoża <rogoza.piotr@gmail.com>
2017-12-09 01:52:08 +0100

    Fixed script

--- sjp.user.js
+++ sjp.user.js
@@ -1,18 +1,25 @@
 // ==UserScript==
-// @name        sjp.pl
-// @description Ustawia skupienie na kontrolce input i zaznacza wprowadzony tekst
-// @namespace   dracorp.pl
-// @include     http://sjp.pl/*
-// @include     https://sjp.pl/*
-// @version     2
-// @grant       none
-// @updateURL   https://github.com/dracorp/userscripts/raw/master/sjp.user.js
-// @downloadURL https://github.com/dracorp/userscripts/raw/master/sjp.user.js
-// @supportURL  https://github.com/dracorp/userscripts/issues
+// @name           Focus on input
+// @name:pl        Skupienie na wejściu
+// @description    Sets focus on input control, and sets a text
+// @description:pl Ustawia skupienie na kontrolce input i zaznacza wprowadzony tekst
+// @namespace      https://github.com/dracorp/userscripts
+// @include        http://sjp.pl/*
+// @include        https://sjp.pl/*
+// @version        3
+// @grant          none
+// @require        https://code.jquery.com/jquery-3.2.1.slim.min.js
+// @updateURL      https://github.com/dracorp/userscripts/raw/master/sjp.user.js
+// @downloadURL    https://github.com/dracorp/userscripts/raw/master/sjp.user.js
+// @supportURL     https://github.com/dracorp/userscripts/issues
+// @icon           https://raw.githubusercontent.com/dracorp/userscripts/master/images/sjp.png
 // ==/UserScript==
 
-window.onload = function(){
-    var input = document.getElementById("sl");
-    input.focus();
-    input.select();
-};
+(function() {
+    'use strict';
+    var input = $('input:text');
+    input[0].focus();
+    input[0].select();
+    input[0].click();
+})();
+
